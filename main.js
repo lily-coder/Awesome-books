65b0e5dde8823bbedec8377bd26b76b03feed912
lily-coder <lilyondie@gmail.com>
2021-08-09 17:50:29 +0300

    Create a function to display books

--- main.js
+++ main.js
@@ -6,9 +6,24 @@ const button = document.querySelector('.btn');
 button.addEventListener('click', function(){
     books.push({title: title.value, author: author.value })
 
+    addBookList();
+})
+
+function addBookList() {
+    const list = document.querySelector('.book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+                    <td>title: ${title.value}</td>
+                    <td>Author: ${author.value}</td>
+                    <td><button type="submit">Remove</button></td>`;
+    
+    list.appendChild(row);
+
     title.value = "";
     author.value = "";
-    console.log(books);
-})
+
+}
 
 
