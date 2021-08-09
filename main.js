73feb2578be1632ff83deaea4df4941f64554707
lily-coder <lilyondie@gmail.com>
2021-08-09 22:41:18 +0300

    Fix linter errors

--- main.js
+++ main.js
@@ -1,29 +1,7 @@
 let books = [];
-const title = document.querySelector(".title");
-const author = document.querySelector(".author");
-const button = document.querySelector(".btn");
-
-//ADD BOOKS TO TABLE LIST
-function addBookList() {
-  const list = document.querySelector(".book-list");
-
-  const row = document.createElement("tr");
-  //RETRIEVE THE BOOK FROM LOCAL STORAGE IN JSON FORMAT
-  window.localStorage.getItem('book');
-  //CONVERT THE JSON FORMAT BACK TO AN OBJECT
-  JSON.parse(window.localStorage.getItem('book'));
-  
-   
-  
-
-  row.innerHTML = `
-                    <td>title: ${title.value}</td>
-                    <td>Author: ${author.value}</td>
-                    <td><button type="submit" class="remove">Remove</button></td>`;
-
-  list.appendChild(row);
-  
-}
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const button = document.querySelector('.btn');
 
 //REMOVE BOOK FUNCTION
 function deleteBook(el) {
@@ -34,14 +12,32 @@ function deleteBook(el) {
 
 //ADD INPUT VALUE
 button.addEventListener("click", function () {
-    books.push({ title: title.value, author: author.value });
+  books.push({ title: title.value, author: author.value });
 
-    addBookList();
-    window.localStorage.setItem('book', JSON.stringify(books));
-  //CLEAR FIELDS AFTER ADD
-    title.value = "";
-    author.value = "";
+  addBookList();
+  window.localStorage.setItem("book", JSON.stringify(books));
 });
+
+//ADD BOOKS TO TABLE LIST
+function addBookList() {
+  const list = document.querySelector(".book-list");
+
+  const row = document.createElement("tr");
+  //RETRIEVE THE BOOK FROM LOCAL STORAGE IN JSON FORMAT
+  window.localStorage.getItem("book");
+  //CONVERT THE JSON FORMAT BACK TO AN OBJECT
+  JSON.parse(window.localStorage.getItem("book"));
+
+  row.innerHTML = `
+                      <td>title: ${title.value}</td>
+                      <td>Author: ${author.value}</td>
+                      <td><button type="submit" class="remove">Remove</button></td>`;
+
+  list.appendChild(row);
+  //CLEAR FIELDS AFTER ADD
+  title.value = "";
+  author.value = "";
+}
 
 //REMOVE BOOK
 const removeBtn = document.querySelector(".book-list");
@@ -49,3 +45,5 @@ const removeBtn = document.querySelector(".book-list");
 removeBtn.addEventListener("click", (e) => {
   deleteBook(e.target);
 });
+
+list.onload = addBookList();
