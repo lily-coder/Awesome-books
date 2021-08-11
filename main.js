b9d52da0e8500ec09a2c37491facd0d334e5ff47
lily-coder <lilyondie@gmail.com>
2021-08-11 17:44:00 +0300

    Fix linter errors

--- main.js
+++ main.js
@@ -1,48 +1,54 @@
+/* eslint no-unused-vars: 0 no-undef: 0 */
 const title = document.querySelector('.title');
 const author = document.querySelector('.author');
 const button = document.querySelector('.btn');
 
-class Book{
-  constructor( title, author){
+class Book {
+  constructor(title, author) {
     this.title = title;
     this.author = author;
   }
 
   // REMOVE BOOK FUNCTION
-  
 
   displayBookList() {
+    this.title = title;
     const list = document.querySelector('.book-list');
     list.innerHTML = '';
-    
-    // RETRIEVE THE BOOK FROM LOCAL STORAGE IN JSON FORMAT  
+
+    // RETRIEVE THE BOOK FROM LOCAL STORAGE IN JSON FORMAT
     localStorage.getItem('book');
+
     // CONVERT THE JSON FORMAT BACK TO AN OBJECT
-    let bookArray = JSON.parse(localStorage.getItem('book') || '[]');
-    //DESTRUCTURE THE STORED DATA IN THE OBJECT PARSED FROM THE LOCALSTORAGE
-  
-    bookArray.forEach(({title, author, id}) => {
+    const bookArray = JSON.parse(localStorage.getItem('book') || '[]');
+
+    // DESTRUCTURE THE STORED DATA IN THE OBJECT PARSED FROM THE LOCALSTORAGE
+
+    bookArray.forEach(({ title, author, id }) => {
       const row = document.createElement('tr');
       row.innerHTML = `
                         <td>title: ${title}</td>
                         <td>Author: ${author}</td>
-                        <td><button type="submit" class="remove" id = "${id}">Remove</button></td>`;
-  
+                        <td><button type='submit' class='remove' id = '${id}'>Remove</button></td>`;
+
       list.appendChild(row);
     });
-  }  
+  }
 
-  //DELETE BOOK 
+  // DELETE BOOK
   deleteBook(el) {
     const books = JSON.parse(localStorage.getItem('book'));
     localStorage.setItem('book', JSON.stringify(books));
-    const id = el.id
-    localStorage.setItem('book', JSON.stringify(books.filter(book => book.id !== id)));
-    this.displayBookList()
+    const { id } = el;
+    localStorage.setItem(
+      'book',
+      JSON.stringify(books.filter((book) => book.id !== id)),
+    );
+    this.displayBookList();
   }
 }
 
-//CREATE A NEW INSTANCE OF BOOK
+// CREATE A NEW INSTANCE OF BOOK
 const book = new Book(title, author);
 
 // ADD INPUT VALUE
@@ -51,11 +57,11 @@ button.addEventListener('click', () => {
   const id = Math.random().toString(36).substr(0, 5);
   books.push({ id, title: title.value, author: author.value });
   localStorage.setItem('book', JSON.stringify(books));
-  
+
   // CLEAR FIELDS AFTER ADD
   title.value = '';
   author.value = '';
-  book.displayBookList()
+  book.displayBookList();
 });
 
 // REMOVE BOOK
@@ -64,6 +70,5 @@ const removeBtn = document.querySelector('.book-list');
 removeBtn.addEventListener('click', (e) => {
   book.deleteBook(e.target);
 });
-
 
 book.displayBookList();
